b6144bab241a5628b2068dd9489723deee6fe2e1
loysevic <161780871+loysevic@users.noreply.github.com>
2024-10-17 22:14:44 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,36 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil?',
+    'A capital do Brasil é Brasilia'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz tchau em Inglês?',
+    'Tchau em ingles é BYE (BAI)'
+)
+
+criaCartao(
+    'Programação',
+    'O que significa depuração (debugging)?',
+    'Depuração é o processo de identificar e corrigir erros ou bugs no código de um programa'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual é a soma dos ângulos internos de um triângulo?',
+    'A soma dos ângulos internos de um triângulo é sempre 180 graus.'
+
+)
